6aeb828360f0af41a25271adb4afc05a66e9a00f
alexanderhodes <alexander.hodes@live.com>
2022-10-09 10:39:50 +0200

    feat: added config

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { Result } from './src/types/result.type';
+import { PORT, TOPIC, YEAR } from './src/config';
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
   const result: Result = {
@@ -17,6 +17,6 @@ app.get('/', (req: Request, res: Response) => {
   res.send(result);
 });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
---
+++ src/config/index.ts
@@ -0,0 +1,3 @@
+export const PORT = process.env.PORT ?? 3000;
+export const YEAR = process.env.YEAR ?? "2013";
+export const TOPIC = process.env.TOPIC ?? "Internal Security";
